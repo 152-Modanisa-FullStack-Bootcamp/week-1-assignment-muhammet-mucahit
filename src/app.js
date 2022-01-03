0d75c8dad43d4adfbafc34b8aeee5ae52b4560df
Mucahit Aktepe <mucahitaktepe@gmail.com>
2022-01-03 03:51:03 +0300

    Complete js task

--- src/app.js
+++ src/app.js
@@ -1,24 +1,34 @@
 import "./styles.css";
 import axios from "axios";
 
+const getProductCard = ({name, image}) =>
+  // Return one product card html with name and image information
+  `<div class="product-card">
+     <img src="${image}" alt="image-alt">
+     <h3>${name}</h3>
+   </div>`;
+
 axios
   .get("https://my-json-server.typicode.com/modanisatech/bootcamp-db/products")
   .then((response) => {
-    // Firstly, log response to the console,
+    console.log(response)
     // inspect the response and see that it has data field
 
     // Assign data field of the response to
     // products variable below by destructuring
     // You can use alias
-    const products = null;
+    const products = response.data;
 
     // Print names of all product to the console
     // by calling foreach  method (use arrow function)
+    products.forEach((item) => console.log(item.name));
 
     // Get all products that contain "Şal" in their name (use filter method)
     // map filtered products to new object having only image and name field
     // assign mapped items to mappedProducts variable
-    const mappedProducts = null;
+    const mappedProducts = products
+      .filter(item => item.name.includes("Şal"))
+      .map(item => ({name: item.name, image: item.image}));
 
     // Display the images and names of mappedProducts
     // You need to add them to the DOM
@@ -26,4 +36,8 @@ axios
     // You need to use flexbox
     // Position of image and text is up to you
     // You can use any style you wish
+    let productsArrayHtml = ""
+    mappedProducts.forEach(item => productsArrayHtml += getProductCard(item));
+    let productsDiv = document.getElementById("products");
+    productsDiv.innerHTML = productsArrayHtml
   });
